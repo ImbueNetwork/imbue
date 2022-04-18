5781f32ff526f3041585e4fffbb4532f5394c9ff
Kanthan <aalavanthan18@gmail.com>
2022-04-19 03:31:37 +1000

    Refactored code and added cancel round. Encountering exception RoundStarted

--- pallets/proposals/src/benchmarking.rs
+++ pallets/proposals/src/benchmarking.rs
@@ -40,12 +40,12 @@ benchmarks! {
     }
 
     schedule_round {
+<<<<<<< HEAD
+        create_project_common::<T>(CONTRIBUTION);
+=======
 
         let caller: T::AccountId = whitelisted_caller();
 
-        let a in 1 .. 100;
-        let caller: T::AccountId = whitelisted_caller();
-        let project_name: Vec<u8> = str::from_utf8(b"Imbue's Awesome Initiative").unwrap().as_bytes().to_vec();
         let project_logo: Vec<u8> = str::from_utf8(b"Imbue Logo").unwrap().as_bytes().to_vec();
         let project_description: Vec<u8> = str::from_utf8(b"This project is aimed at promoting Decentralised Data and Transparent Crowdfunding.").unwrap().as_bytes().to_vec();
         let website: Vec<u8> = str::from_utf8(b"https://imbue.network").unwrap().as_bytes().to_vec();
@@ -56,15 +56,17 @@ benchmarks! {
 
         let required_funds: BalanceOf<T> = 100u32.into();
         let currency_id = CurrencyId::Native;
+>>>>>>> 5840e0bf3834731fd4271fd32ef649e41adc1617
         let start_block: T::BlockNumber = 0u32.into();
-
         let end_block: T::BlockNumber = 10u32.into();
         let project_key: Vec<ProjectKey> = vec![0];
-        Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id)?;
 
-    }: _(RawOrigin::Root, start_block, end_block, project_key)
+<<<<<<< HEAD
     verify {
-       // assert_last_event::<T>(Event::FundingRoundCreated(0).into());
+        assert_last_event::<T>(Event::FundingRoundCreated(0).into());
+
+    }: _(RawOrigin::Root, 0)
+    verify {
     }
 
 
@@ -87,4 +89,28 @@ where
     assert_eq!(event, &system_event);
 }
 
+<<<<<<< HEAD
+fn create_project_common<T: Config>(projectKey: u32){
+        let caller: T::AccountId = whitelisted_caller();
+        let project_name: Vec<u8> = str::from_utf8(b"Imbue's Awesome Initiative").unwrap().as_bytes().to_vec();
+        let project_logo: Vec<u8> = str::from_utf8(b"Imbue Logo").unwrap().as_bytes().to_vec();
+        let project_description: Vec<u8> = str::from_utf8(b"This project is aimed at promoting Decentralised Data and Transparent Crowdfunding.").unwrap().as_bytes().to_vec();
+        let website: Vec<u8> = str::from_utf8(b"https://imbue.network").unwrap().as_bytes().to_vec();
+        let milestones: Vec<ProposedMilestone> = vec![ProposedMilestone {
+            name: Vec::new(),
+            percentage_to_unlock: 100,
+        }];
+
+        let required_funds: BalanceOf<T> = 100u32.into();
+        let currency_id = CurrencyId::Native;
+        
+        let start_block: T::BlockNumber = 0u32.into();
+
+        Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id);
+}
+
+
 impl_benchmark_test_suite!(Proposals, crate::mock::new_test_ext(), crate::mock::Test);
+=======
+impl_benchmark_test_suite!(Proposals, crate::mock::new_test_ext(), crate::mock::Test);
+>>>>>>> 5840e0bf3834731fd4271fd32ef649e41adc1617
