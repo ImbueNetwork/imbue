306f220edab5975d185c0fed7e9a9f8722bcceaf
Kanthan <aalavanthan18@gmail.com>
2022-04-18 23:24:13 +1000

    Cancel round benchmark added. Hitting the 'RoundCreated' exception

--- pallets/proposals/src/benchmarking.rs
+++ pallets/proposals/src/benchmarking.rs
@@ -13,6 +13,7 @@ use sp_std::str;
 use sp_std::vec::Vec;
 use common_types::CurrencyId;
   
+const CONTRIBUTION: u32 = 400;
 
 benchmarks! {
     where_clause { where
@@ -40,44 +41,29 @@ benchmarks! {
     }
     
     schedule_round {
-
-        let caller: T::AccountId = whitelisted_caller();
-        let name = str::from_utf8(b"Imbue's Awesome Initiative").unwrap().as_bytes().to_vec();
-        let logo = str::from_utf8(b"Imbue Logo").unwrap().as_bytes().to_vec();
-        let description = str::from_utf8(b"This project is aimed at promoting Decentralised Data and Transparent Crowdfunding.").unwrap().as_bytes().to_vec();
-        let website = str::from_utf8(b"https://imbue.network").unwrap().as_bytes().to_vec();
-        let milestones = vec![ProposedMilestone {
-            name: Vec::new(),
-            percentage_to_unlock: 100,
-        }];
-        let required_funds = 1000000u64;
-        let currency_id = CurrencyId::Native;
-        
+        create_project_common::<T>(CONTRIBUTION);
         let start_block: T::BlockNumber = 0u32.into();
-
-        //let project = Proposals::Project {
-        let project = Project {
-            name: name.clone(),
-            logo,
-            description,
-            website,
-            milestones,
-            contributions: Vec::new(),
-            required_funds,
-            currency_id,
-            withdrawn_funds: (0_u32).into(),
-            initiator: caller.clone(),
-            create_block_number: start_block,
-            approved_for_funding: false,
-        };
-
-        <Projects<T>>::insert(0, project);
-
-        
         let end_block: T::BlockNumber = 10u32.into();
         let project_key: Vec<ProjectKey> = vec![0];
 
     }: _(RawOrigin::Root, start_block, end_block, project_key)
+    verify {
+        assert_last_event::<T>(Event::FundingRoundCreated(0).into());
+    }
+
+    cancel_round {
+        
+        let caller: T::AccountId = whitelisted_caller();
+        //Setting the start block to be greater than 0 which is the current block. 
+        //This condition is checked to ensure the round being cancelled has not started yet.
+        let start_block: T::BlockNumber = 1u32.into();
+        let end_block: T::BlockNumber = 10u32.into();
+        let project_key: Vec<ProjectKey> = vec![0];
+        
+        create_project_common::<T>(CONTRIBUTION);
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+
+    }: _(RawOrigin::Root, 0)
     verify {
        // assert_last_event::<T>(Event::FundingRoundCreated(0).into());
     }
@@ -97,6 +83,25 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) where <T as
 	// compare to the last event record
 	let EventRecord { event, .. } = &events[events.len() - 1];
 	assert_eq!(event, &system_event);
+}
+
+fn create_project_common<T: Config>(projectKey: u32){
+        let caller: T::AccountId = whitelisted_caller();
+        let project_name: Vec<u8> = str::from_utf8(b"Imbue's Awesome Initiative").unwrap().as_bytes().to_vec();
+        let project_logo: Vec<u8> = str::from_utf8(b"Imbue Logo").unwrap().as_bytes().to_vec();
+        let project_description: Vec<u8> = str::from_utf8(b"This project is aimed at promoting Decentralised Data and Transparent Crowdfunding.").unwrap().as_bytes().to_vec();
+        let website: Vec<u8> = str::from_utf8(b"https://imbue.network").unwrap().as_bytes().to_vec();
+        let milestones: Vec<ProposedMilestone> = vec![ProposedMilestone {
+            name: Vec::new(),
+            percentage_to_unlock: 100,
+        }];
+
+        let required_funds: BalanceOf<T> = 100u32.into();
+        let currency_id = CurrencyId::Native;
+        
+        let start_block: T::BlockNumber = 0u32.into();
+
+        Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id);
 }
 
 
