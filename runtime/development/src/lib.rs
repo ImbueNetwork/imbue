574dad5d0aa8d49f2e7639cce6e7744b81e786b2
sam elamin <hussam.elamin@gmail.com>
2022-05-12 21:02:00 +0100

    milestone approval bug

--- runtime/development/src/lib.rs
+++ runtime/development/src/lib.rs
@@ -118,7 +118,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("imbue-dev"),
     impl_name: create_runtime_str!("imbue-dev"),
     authoring_version: 1,
-    spec_version: 1002,
+    spec_version: 1004,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
