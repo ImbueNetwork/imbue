75bbd2c4923505cc048fadc14493d7ca12a6172e
sam elamin <hussam.elamin@gmail.com>
2022-05-26 10:40:05 +0100

    add round type to schedule round

--- pallets/proposals/src/benchmarking.rs
+++ pallets/proposals/src/benchmarking.rs
@@ -47,7 +47,7 @@ benchmarks! {
         let end_block: T::BlockNumber = 10u32.into();
         let project_keys: Vec<ProjectKey> = vec![0];
 
-    }: _(RawOrigin::Root, start_block, end_block, project_keys.clone())
+    }: _(RawOrigin::Root, start_block, end_block, project_keys.clone(), RoundType::ContributionRound)
     verify {
         assert_last_event::<T>(Event::FundingRoundCreated(0, project_keys).into());
     }
@@ -65,7 +65,7 @@ benchmarks! {
         //create project
         create_project_common::<T>(CONTRIBUTION);
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
 
     }: _(RawOrigin::Root, 0)
     verify {
@@ -91,7 +91,7 @@ benchmarks! {
         //create project
         create_project_common::<T>(CONTRIBUTION);
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks
         run_to_block::<T>(progress_block_number);
 
@@ -118,7 +118,7 @@ benchmarks! {
         //create project
         create_project_common::<T>(CONTRIBUTION);
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks
         run_to_block::<T>(progress_block_number);
         //contribute
@@ -150,7 +150,7 @@ benchmarks! {
         create_project_common::<T>(CONTRIBUTION);
         //Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id)?;
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks
         run_to_block::<T>(progress_block_number);
         //contribute
@@ -186,7 +186,7 @@ benchmarks! {
         create_project_common::<T>(CONTRIBUTION);
         //Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id)?;
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks - to a block after the round start block for the project
         run_to_block::<T>(progress_block_number_contribute);
         //contribute
@@ -230,7 +230,7 @@ benchmarks! {
         create_project_common::<T>(CONTRIBUTION);
         //Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id)?;
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks - to a block after the round start block for the project
         run_to_block::<T>(progress_block_number_contribute);
         //contribute
@@ -279,7 +279,7 @@ benchmarks! {
         create_project_common::<T>(CONTRIBUTION);
         //Proposals::<T>::create_project(RawOrigin::Signed(caller.clone()).into(), project_name.clone(), project_logo, project_description, website, milestones, required_funds, currency_id)?;
         //schedule round
-        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key)?;
+        Proposals::<T>::schedule_round(RawOrigin::Root.into(), start_block, end_block, project_key, RoundType::ContributionRound)?;
         //progress the blocks - to a block after the round start block for the project
         run_to_block::<T>(progress_block_number_contribute);
         //contribute
