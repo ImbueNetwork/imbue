beba4dbd92d7419ecef77b62d474cf8ae774f8a7
f-gate <felixgate13@gmail.com>
2023-04-17 00:15:11 +0800

    some meat

--- pallets/grant/src/lib.rs
+++ pallets/grant/src/lib.rs
@@ -12,12 +12,21 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::*,
+		BoundedVec,
+		BoundedBTreeMap,
+	};
 	use frame_system::pallet_prelude::*;
 	use orml_traits::{MultiCurrency, MultiReservableCurrency};
+	use orml_traits::arithmetic::Bounded;
 	use common_types::CurrencyId;
-	
-	pub type BalanceOf<T> = <<T as Config>::RMultiCurrency as MultiCurrency<AccountIdOf<T>>>::Balance;
+	use sp_runtime::traits::AtLeast32BitUnsigned;
+
+	pub(crate) type BalanceOf<T> = <<T as Config>::RMultiCurrency as MultiCurrency<AccountIdOf<T>>>::Balance;
+	pub(crate) type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+	type BoundedPMilestones<T> = BoundedVec<ProposedMilestoneWithInfo, <T as Config>::MaxMilestonesPerGrant>;
+	type BoundedApprovers<T> = BoundedVec<AccountIdOf<T>, <T as Config>::MaxApprovers>;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -26,62 +35,132 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-        type MultiCurrency: RMultiReservableCurrency<AccountIdOf<Self>, CurrencyId = CurrencyId>;
+        type RMultiCurrency: MultiReservableCurrency<AccountIdOf<Self>, CurrencyId = CurrencyId>;
+		/// The grant ID type.
+		type GrantId: Parameter + Member + AtLeast32BitUnsigned + Default + Copy + MaybeSerializeDeserialize + Bounded + codec::FullCodec + MaxEncodedLen;
+		type MaxMilestonesPerGrant: Get<u32>;
+		type MaxApprovers: Get<u32>;
+	
+		// TODO: Use this for the voting window on grant creation.
+		// If its passed the voting period check in hook and remove the grant.
+		type GrantVotingPeriod: Get<<Self as frame_system::Config>::BlockNumber>;
 	}
 
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	pub type PendingGrants<T> = ;
+	pub type PendingGrants<T: Config> = StorageMap<_, Blake2_128, T::GrantId, Grant<T>, OptionQuery>;
+
+	//TODO: Come up with efficient way of holding votes.
+	//#[pallet::storage]
+	//#[pallet::getter(fn something)]
+	//pub type GrantVotes<T: Config> = StorageMap<_, Blake2_128, T::GrantId, BTreeMap>;
+
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+		GrantSubmitted{submitter: AccountIdOf<T>, grant_id: T::GrantId},
+		GrantVotedUpon{voter: AccountIdOf<T>, grant_id: T::GrantId, way: VoteType},
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
+		/// Milestones must sum to 100
+		MustSumTo100,
+		/// The GrantId specified cannot be found.
+		GrantNotFound,
+		/// Only appointed approvers and vote on a grant submission.
+		OnlyApproversCanVote,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
         
+		/// A grant starts here with nothing agreed upon and 
+		/// probably awaiting much back and forth.
 		#[pallet::call_index(0)]
         #[pallet::weight(100_000)]
         pub fn submit_initial_grant(
             origin: OriginFor<T>,
 			ipfs_hash: [u8; 32],
-			
+			proposed_milestones: BoundedPMilestones<T>,
+			assigned_approvers: BoundedApprovers<T>,
         ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
-			// take deposit to prevent spam
+            let submitter = ensure_signed(origin)?;
+			// TODO: take deposit to prevent spam? how else can we prevent spam
+			let total_percentage = proposed_milestones.iter().fold(0u32, |acc, x| acc.saturating_add(x.percent.into()));
+			ensure!(total_percentage == 100, Error::<T>::MustSumTo100);
+			
+			// TODO: Ensure that the approvers are in a select group??
+			//ensure!()
 
-            Self::deposit_event(Event::GrantSubmitted(now));
+			// TODO: GENERATE grant_id. properly. or get as param
+			let grant_id: T::GrantId = Default::default();
+			
+			let grant = Grant {
+				milestones: proposed_milestones,
+				submitter: submitter.clone(),
+				approvers: assigned_approvers
+			};
 
+			PendingGrants::<T>::insert(&grant_id, grant);
+			// TODO: Add to the voting expiration window.
+
+            Self::deposit_event(Event::<T>::GrantSubmitted{submitter, grant_id});
             Ok(().into())
         }
 
+		/// For the people approving, they must submit the vote to accept or decline the grant proposal. 
 		#[pallet::call_index(1)]
         #[pallet::weight(100_000)]
         pub fn vote_on_grant(
             origin: OriginFor<T>,
+			vote: VoteType,
+			grant_id: T::GrantId,
         ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
-			// take deposit to prevent spam
+            let voter = ensure_signed(origin)?;
+			let grant: Grant<T> = PendingGrants::<T>::get(grant_id).ok_or(Error::<T>::GrantNotFound)?;
 
-            Self::deposit_event(Event::GrantSubmitted(now));
+			ensure!(grant.approvers.iter().any(|approver|approver == &voter), Error::<T>::OnlyApproversCanVote);
+			// generate a vote key
+			// insert into vote storage
+			// add hook to see if at the start of the block there is a super majority and close.
 
+            Self::deposit_event(Event::<T>::GrantVotedUpon{voter, grant_id, way: vote});
             Ok(().into())
         }
 
 		#[pallet::call_index(2)]
         #[pallet::weight(100_000)]
-        pub fn commence_work(
+        pub fn convert_to_milestones(
             origin: OriginFor<T>,
         ) -> DispatchResultWithPostInfo {
-
+			// Some method (that will eventually do the same thing as the brief evolver)
+			// and allow for the submission of these milestones.
+			Ok(().into())
         }
-
-		// RUNTIME API TO GET THE DEPOSIT ADDRESS FOR THE GRANT ON APPROVAL
-
+		// TODO: runtime api to get the deposit address of the grant sovereign account.
 	}
+
+	#[derive(Encode, Decode, PartialEq, Eq, Clone, Debug, MaxEncodedLen, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Grant<T: Config> {
+		milestones: BoundedPMilestones<T>,
+		submitter: AccountIdOf<T>,
+		approvers: BoundedApprovers<T>,
+	}
+	
+	#[derive(Encode, Decode, PartialEq, Eq, Clone, Debug, MaxEncodedLen, TypeInfo)]
+	pub enum VoteType {
+		Approve,
+		ChangesRequested,
+		Cancel,
+	}
+
+	#[derive(Encode, Decode, PartialEq, Eq, Clone, Debug, MaxEncodedLen, TypeInfo)]
+	pub struct ProposedMilestoneWithInfo {
+		percent: u8,
+		ipfs_hash: [u8; 32],
+	}
+
 }
--- pallets/grant/src/tests.rs
+++ pallets/grant/src/tests.rs
@@ -2,7 +2,7 @@ use crate::{mock::*, Error, Event};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
+fn ensure_milestone_percent_equal_100() {
 	new_test_ext().execute_with(|| {
 		// Go past genesis block so events get deposited
 		System::set_block_number(1);
