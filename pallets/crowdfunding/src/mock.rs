67f82722c2d8738f036a414fd3495aef83df86c6
f-gate <felixgate13@gmail.com>
2023-05-12 16:12:05 +0100

    mock

--- pallets/crowdfunding/src/mock.rs
+++ pallets/crowdfunding/src/mock.rs
@@ -1,13 +1,20 @@
 use crate as pallet_crowdfunding;
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::traits::{ConstU16, ConstU64, Nothing};
+use frame_support::parameter_types;
 use sp_core::H256;
+use sp_core::ed25519::Signature;
 use sp_runtime::{
 	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
+	traits::{BlakeTwo256, IdentityLookup, IdentifyAccount, Verify}
 };
+use frame_system::EnsureRoot;
+use common_types::CurrencyId;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
+type BlockNumber = u32;
+type Balance = u32;
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -18,6 +25,9 @@ frame_support::construct_runtime!(
 	{
 		System: frame_system,
 		CrowdFunding: pallet_crowdfunding,
+		Tokens: orml_tokens,
+		Identity: pallet_identity,
+		Balances: pallet_balances,
 	}
 );
 
@@ -29,17 +39,17 @@ impl frame_system::Config for Test {
 	type RuntimeOrigin = RuntimeOrigin;
 	type RuntimeCall = RuntimeCall;
 	type Index = u64;
-	type BlockNumber = u64;
+	type BlockNumber = BlockNumber;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
-	type AccountId = u64;
+	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type RuntimeEvent = RuntimeEvent;
 	type BlockHashCount = ConstU64<250>;
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+    type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -48,9 +58,92 @@ impl frame_system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
+parameter_types! {
+	pub RoundExpiry: BlockNumber = 100;
+	pub MaxKeysPerRound: u32 = 50;
+	pub MaxContributionsPerCrowdFund: u32 = 1000;
+	pub MaxMilestonesPerCrowdFund: u32 = 100;
+	pub MaxWhitelistPerCrowdFund: u32 = 100;
+	pub IsIdentityRequired: bool = false;
+}
+
 impl pallet_crowdfunding::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	type MultiCurrency = Tokens;
+	type RoundExpiry = RoundExpiry;
+	type MaxKeysPerRound = MaxKeysPerRound;
+	type MaxContributionsPerCrowdFund = MaxContributionsPerCrowdFund;
+	type MaxMilestonesPerCrowdFund = MaxMilestonesPerCrowdFund;
+	type MaxWhitelistPerCrowdFund = MaxWhitelistPerCrowdFund;
+	type IsIdentityRequired = IsIdentityRequired;
+	type AuthorityOrigin = EnsureRoot<Self::RuntimeOrigin>;
 }
+
+orml_traits::parameter_type_with_key! {
+    pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+        100
+    };
+}
+parameter_types! {
+    pub const MaxReserves: u32 = 50;
+    pub MaxLocks: u32 = 2;
+
+}
+impl orml_tokens::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type Balance = Balance;
+    type Amount = i128;
+    type CurrencyId = common_types::CurrencyId;
+    type CurrencyHooks = ();
+    type WeightInfo = ();
+    type ExistentialDeposits = ExistentialDeposits;
+    type MaxLocks = MaxLocks;
+    type DustRemovalWhitelist = Nothing;
+    type MaxReserves = MaxReserves;
+    type ReserveIdentifier = [u8; 8];
+}
+
+parameter_types! {
+    pub const BasicDeposit: u64 = 10;
+    pub const FieldDeposit: u64 = 10;
+    pub const SubAccountDeposit: u64 = 10;
+    pub const MaxSubAccounts: u32 = 2;
+    pub const MaxAdditionalFields: u32 = 2;
+    pub const MaxRegistrars: u32 = 20;
+}
+
+impl pallet_identity::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type Currency = Balances;
+    type Slashed = ();
+    type BasicDeposit = BasicDeposit;
+    type FieldDeposit = FieldDeposit;
+    type SubAccountDeposit = SubAccountDeposit;
+    type MaxSubAccounts = MaxSubAccounts;
+    type MaxAdditionalFields = MaxAdditionalFields;
+    type MaxRegistrars = MaxRegistrars;
+    type RegistrarOrigin = EnsureRoot<AccountId>;
+    type ForceOrigin = EnsureRoot<AccountId>;
+    type WeightInfo = ();
+}
+
+parameter_types! {
+    pub const ExistentialDeposit: u64 = 5;
+}
+
+impl pallet_balances::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type AccountStore = System;
+    type Balance = u64;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
+}
+
+
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
