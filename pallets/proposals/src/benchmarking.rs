c3d397f6bc4a083d86a00739783318cc6834d9b4
cuteolaf <oliverlim818@gmail.com>
2023-05-30 12:40:29 +0200

    vote_on_milestone

--- pallets/proposals/src/benchmarking.rs
+++ pallets/proposals/src/benchmarking.rs
@@ -3,6 +3,7 @@ use super::*;
 use crate::test_utils::*;
 use crate::Pallet as Proposals;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, vec};
+use frame_support::assert_ok;
 use frame_system::RawOrigin;
 
 benchmarks! {
@@ -20,6 +21,21 @@ benchmarks! {
     }: _(RawOrigin::Signed(bob.clone()), project_key, 0)
     verify{
         assert_last_event::<T>(Event::<T>::VotingRoundCreated(project_key).into());
+    }
+
+    vote_on_milestone {
+        let alice: T::AccountId = create_funded_user::<T>("initiator", 1, 1_000);
+        let bob: T::AccountId = create_funded_user::<T>("contributor", 1, 1_000);
+        let contributions = get_contributions::<T>(vec![bob.clone()], 500);
+        let prop_milestones = get_max_milestones::<T>();
+        let project_key = create_project::<T>(alice.clone(), contributions, prop_milestones, CurrencyId::Native);
+
+        assert_ok!(Proposals::<T>::submit_milestone(RawOrigin::Signed(alice).into(), project_key, 0));
+        // Contributor, ProjectKey, MilestoneKey, ApproveMilestone
+    }: _(RawOrigin::Signed(bob.clone()), project_key, 0, true)
+    verify {
+        let current_block: T::BlockNumber = frame_system::Pallet::<T>::block_number();
+        assert_last_event::<T>(Event::<T>::VoteSubmitted(bob, project_key, 0, true, current_block).into())
     }
 }
 
