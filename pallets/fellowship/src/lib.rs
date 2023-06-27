9578780dc20ddac211afdfecc32cf6ade486abcb
f-gate <felixgate13@gmail.com>
2023-06-27 11:13:35 +0100

    fix build

--- pallets/fellowship/src/lib.rs
+++ pallets/fellowship/src/lib.rs
@@ -14,16 +14,18 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*, BoundedBTreeMap};
 	use frame_system::pallet_prelude::*;
-	use sp_std::collections::btree::BTreeMap;
-	use orml_traits::MultiReservableCurrency;
+	use orml_traits::{MultiReservableCurrency, MultiCurrency, BalanceStatus};
 	use common_types::CurrencyId;
 	use sp_runtime::traits::BadOrigin;
+	use sp_std::convert::TryInto;
 
-	type AccountIdOf<T> = <T as Config::frame_system>::AccountId;
+	use crate::traits::{EnsureRole, FellowshipHandle, DemocracyHandle};
+
+	type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 	type BalanceOf<T> = <<T as Config>::MultiCurrency as MultiCurrency<AccountIdOf<T>>>::Balance;
-	type ShortlistRound = u32;
+	type ShortlistRoundKey = u32;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -34,13 +36,13 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
         type MultiCurrency: MultiReservableCurrency<AccountIdOf<Self>, CurrencyId = CurrencyId>;
 		/// The authority appropriate to do call force extrinsics.
-		type ForceAuthority: EnsureOrigin<AccountIdOf<Self>>;
+		type ForceAuthority: EnsureOrigin<<Self as frame_system::Config>::RuntimeOrigin>;
 		/// The handle used to initiate democracy calls.
-		type DemocracyHandle: traits::DemocracyHandle<AccountIdOf<Self>>;
+		type DemocracyHandle: DemocracyHandle<AccountIdOf<Self>>;
 		/// The max number of candidates per wave.
 		type MaxCandidatesPerShortlist: Get<u32>;
 		/// The amount of time before a shortlist is moved to be voted on.
-		type ShortlistPeriod: Get<BlockNumberFor<Self>>
+		type ShortlistPeriod: Get<BlockNumberFor<Self>>;
 		/// The minimum deposit required for a freelancer to hold fellowship status.
 		type MembershipDeposit: Get<BalanceOf<Self>>;
 		/// Currently just send all slash deposits to a single account.
@@ -52,16 +54,16 @@ pub mod pallet {
 	/// Returns the role of the account
     #[pallet::storage]
     pub type Roles<T> =
-        StorageMap<_, Blake2_128Concat, AccountIdOf<T>, T::Role, OptionQuery>;
+        StorageMap<_, Blake2_128Concat, AccountIdOf<T>, Role, OptionQuery>;
 
 	/// Contains the shortlist of candidates to be sent for approval.
 	#[pallet::storage]
     pub type CandidateShortlist<T> =
-        StorageMap<_, ShortlistRound, BoundedBTreeMap<AccountIdOf<T>, Role, <T as Config>::MaxCandidatesPerShortlist>, ValueQuery>;
+        StorageMap<_,  Blake2_128Concat, ShortlistRoundKey, BoundedBTreeMap<AccountIdOf<T>, Role, <T as Config>::MaxCandidatesPerShortlist>, ValueQuery>;
 
 	/// Keeps track of the round the shortlist is in.
 	#[pallet::storage]
-	pub type ShortlistRound<T> = StorageValue<_, ShortlistRound, ValueQuery>;
+	pub type ShortlistRound<T> = StorageValue<_, ShortlistRoundKey, ValueQuery>;
 
 	#[pallet::storage]
 	/// Holds all the accounts that are able to become fellows that have not given their deposit for membership.
@@ -72,7 +74,7 @@ pub mod pallet {
 	/// Needed incase the reserve amount will change.
 	#[pallet::storage]
 	pub type FellowshipReserves<T> =
-		StorageMap<_, Blake2_128Concat, AccountIdOf<T>, BalanceOf<T>, ValueQuery>;
+		StorageMap<_, Blake2_128Concat, AccountIdOf<T>, BalanceOf<T>, OptionQuery>;
 	
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -81,7 +83,8 @@ pub mod pallet {
 		FellowshipRemoved(AccountIdOf<T>),
 		FellowshipSlashed(AccountIdOf<T>),
 		MemberAddedToPendingFellows(AccountIdOf<T>),
-		CandidateAddedToShortlist(AccountIfOf<T>),
+		CandidateAddedToShortlist(AccountIdOf<T>),
+		CandidateRemovedFromShortlist(AccountIdOf<T>),
 	}
 
 	#[pallet::error]
@@ -100,6 +103,8 @@ pub mod pallet {
 		CandidateAlreadyOnShortlist,
 		/// The maximum number of candidates has been reached.
 		TooManyCandidates,
+		/// The fellowship deposit has could not be found, contact development.
+		FellowshipReserveDisapeared,
 	}
 
 	#[pallet::call]
@@ -109,9 +114,9 @@ pub mod pallet {
 		/// Force add someone to the fellowship. This is required to be called by the ForceOrigin
 		#[pallet::call_index(0)]
 		#[pallet::weight(10_000)]
-		pub fn force_add_fellowship(origin: OriginFor<T>, who: AccountIdOf<T>, deposit_sponsor: AccountIdOf<T>) -> DispatchResult {
+		pub fn force_add_fellowship(origin: OriginFor<T>, who: AccountIdOf<T>, role: Role) -> DispatchResult {
             <T as Config>::ForceAuthority::ensure_origin(origin)?;
-			<Self as FellowshipHandler>::add_to_fellowship(&who)?
+			<Self as FellowshipHandle<AccountIdOf<T>>>::add_to_fellowship(&who, role)?;
 			Self::deposit_event(Event::<T>::FellowshipAdded(who));
 			Ok(().into())
 		}
@@ -123,7 +128,7 @@ pub mod pallet {
 		pub fn leave_fellowship(origin: OriginFor<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			// TODO: ensure that the fellow is not currently in a dispute.
-			<Self as FellowshipHandler>::revoke_fellowship(&who, false)?
+			<Self as FellowshipHandle<AccountIdOf<T>>>::revoke_fellowship(&who, false)?;
 			Self::deposit_event(Event::<T>::FellowshipRemoved(who));
 			Ok(().into())
 		}
@@ -131,13 +136,12 @@ pub mod pallet {
 		/// Force remove a fellow and slashed their deposit as defined in the Config.
 		#[pallet::call_index(2)]
 		#[pallet::weight(10_000)]
-		pub fn force_remove_and_slash_fellowship(origin: OriginFor<T>) -> DispatchResult {
+		pub fn force_remove_and_slash_fellowship(origin: OriginFor<T>, who: AccountIdOf<T>) -> DispatchResult {
             <T as Config>::ForceAuthority::ensure_origin(origin)?;
-			<Self as FellowshipHandler>::revoke_fellowship(&who, true)?;
+			<Self as FellowshipHandle<AccountIdOf<T>>>::revoke_fellowship(&who, true)?;
 			Self::deposit_event(Event::<T>::FellowshipSlashed(who));
 			Ok(().into())
 		}
-
 
 		/// Add a candidate to a shortlist. 
 		/// The caller must be of type Vetter to add to a shortlist.
@@ -146,12 +150,12 @@ pub mod pallet {
 		#[pallet::weight(10_000)]
 		pub fn add_candidate_to_shortlist(origin: OriginFor<T>, candidate: AccountIdOf<T>, role: Role) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-			ensure!(EnsureFellowshipRole::<T>::ensure_has_role(&who).is_ok(), Error::<T>::NotAVetter);
+			ensure!(EnsureFellowshipRole::<T>::ensure_role_in(&who, vec![Role::Freelancer, Role::Vetter]).is_ok(), Error::<T>::NotAVetter);
 			ensure!(Roles::<T>::get(&candidate).is_none(), Error::<T>::AlreadyAFellow);
-			ensure(MultiCurrency::can_reserve(CurrencyId::Native, &candidate, <T as Config>::MembershipDeposit::get()), Error::<T>::CandidateDepositRequired);
-			let _ = CandidateShortlist::<T>::try_mutate(ShortlistRound::<T>::get() |m_shortlist| -> DispatchResult {
+			ensure!(T::MultiCurrency::can_reserve(CurrencyId::Native, &candidate, <T as Config>::MembershipDeposit::get()), Error::<T>::CandidateDepositRequired);
+			let _ = CandidateShortlist::<T>::try_mutate(ShortlistRound::<T>::get(), |m_shortlist| {
 				ensure!(!m_shortlist.contains_key(&candidate), Error::<T>::CandidateAlreadyOnShortlist);
-				m_shortlist.try_insert(&candidate).map_err(|_| Error::<T>::TooManyCandidates)?;
+				m_shortlist.try_insert(candidate.clone(), role).map_err(|_| Error::<T>::TooManyCandidates)?;
 				Ok::<(), DispatchError>(())
 			})?;
 
@@ -161,27 +165,24 @@ pub mod pallet {
 
 		#[pallet::call_index(4)]
 		#[pallet::weight(10_000)]
-		pub fn remove_candidate_from_shortlist(origin: OriginFor<T>) -> DispatchResult {
+		pub fn remove_candidate_from_shortlist(origin: OriginFor<T>, candidate: AccountIdOf<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-			ensure!(EnsureFellowshipRole::<T>::ensure_role(&who, Role::Vetter).is_ok(), Error::<T>::NotAVetter);
-			let _ = CandidateShortlist::<T>::try_mutate(ShortlistRound::<T>::get() |m_shortlist| -> DispatchResult {
-				ensure!(!m_shortlist.contains_key(&candidate), Error::<T>::CandidateAlreadyOnShortlist);
-				m_shortlist.try_insert(&candidate).map_err(|_| Error::<T>::TooManyCandidates)?;
+			ensure!(EnsureFellowshipRole::<T>::ensure_role_in(&who, vec![Role::Freelancer, Role::Vetter]).is_ok(), Error::<T>::NotAVetter);
+			let _ = CandidateShortlist::<T>::try_mutate(ShortlistRound::<T>::get(), |m_shortlist| {
+				m_shortlist.remove(&candidate);
 				Ok::<(), DispatchError>(())
 			})?;
 
-			Self::deposit_event(Event::<T>::CandidateAddedToShortlist(candidate));
-			
+			Self::deposit_event(Event::<T>::CandidateRemovedFromShortlist(candidate));
+			Ok(().into())			
 		}
 	}
 
-	impl<T: crate::Config> FellowshipHandler<AccountIdOf<T>> for Pallet<T> {
-		type Role = <T as Config>::Role;
+	impl<T: crate::Config> FellowshipHandle<AccountIdOf<T>> for Pallet<T> {
+		type Role = crate::pallet::Role;
 
-		fn bulk_add_to_fellowship() -> Result<(), DispatchError>{
-			// call add to fellowship with a limit.
-			// remove all those from the CandidateShortlist
-			// TODO: Candidate shortlist should have a map into shortlist version.
+		fn bulk_add_to_fellowship(short_list: Vec<(AccountIdOf<T>, Role)>) -> Result<(), DispatchError>{
+			Ok(())
 		}
 
 		/// Add someone to the fellowship, if this fails to be tried again on demand.
@@ -193,7 +194,7 @@ pub mod pallet {
 			if !Roles::<T>::contains_key(who) {
 				let membership_deposit = <T as Config>::MembershipDeposit::get();
 				if <T as Config>::MultiCurrency::can_reserve(CurrencyId::Native, who, membership_deposit) {
-					<T as Config>::MultiCurrency::reserve(CurrencyId::Native, who, membership_deposit);
+					<T as Config>::MultiCurrency::reserve(CurrencyId::Native, who, membership_deposit)?;
 					FellowshipReserves::<T>::insert(who, membership_deposit);
 					Roles::<T>::insert(who, role);
 				} else {
@@ -209,20 +210,19 @@ pub mod pallet {
 		/// Revoke the fellowship from an account.
 		/// If they have not paid the deposit but are eligable then they can still be revoked
 		/// using this method.
-		fn revoke_fellowship(who: &AccountId, slash_deposit: bool) -> Result<(), DispatchError> {
+		fn revoke_fellowship(who: &AccountIdOf<T>, slash_deposit: bool) -> Result<(), DispatchError> {
 			let has_role = Roles::<T>::contains_key(who);
-			ensure!(PendingFellows::<T>::contains_key(who) || has_role, NotAFellow);
+			ensure!(PendingFellows::<T>::contains_key(who) || has_role, Error::<T>::NotAFellow);
 			PendingFellows::<T>::remove(who);
 			Roles::<T>::remove(who);
-
-			let deposit_amount: BalanceOf<T> = <T as Config>::MembershipDeposit::get();
 			// Essentially you can only slash a deposit if it has been taken
 			// Deposits are only taken when a role is assigned
 			if has_role {
+				let deposit_amount: BalanceOf<T> = FellowshipReserves::<T>::get(&who).ok_or(Error::<T>::FellowshipReserveDisapeared)?;
 				if slash_deposit {
 					let _imbalance = <T as Config>::MultiCurrency::repatriate_reserved(
 						CurrencyId::Native,
-						who,
+						&who,
 						&<T as Config>::SlashAccount::get(),
 						deposit_amount,
 						BalanceStatus::Free,
@@ -243,25 +243,26 @@ pub mod pallet {
 		type Success = ();
 		
 		fn ensure_role(acc: &AccountIdOf<T>, role: Role) -> Result<Self::Success, BadOrigin> {
-			let actual = Roles::<T>::get(acc).ok_or(BadOrigin)?
+			let actual = Roles::<T>::get(acc).ok_or(BadOrigin)?;
 			if role == actual {
 				Ok(())
 			} else {
-				Err(Error::<T>::BadOrigin)
+				Err(BadOrigin)
 			}
 		}
 
-		fn ensure_role_in(acc: &AccountId, roles: Vec<Roles>) -> Result<Self::Success, BadOrigin> {
-			let role = Roles::<T>::get(acc).ok_or(BadOrigin);
-			ensure!(roles.contains(role), BadOrigin);
+		fn ensure_role_in(acc: &AccountIdOf<T>, roles: Vec<Role>) -> Result<Self::Success, BadOrigin> {
+			let role = Roles::<T>::get(acc).ok_or(BadOrigin)?;
+			ensure!(roles.contains(&role), BadOrigin);
 			Ok(())
 		}
 	}
 
-    #[derive(Encode, Decode, PartialEq, Eq, Clone, Debug, MaxEncodedLen, TypeInfo)]
+    #[derive(Encode, Decode, PartialEq, Eq, Copy, Clone, Debug, MaxEncodedLen, TypeInfo)]
 	pub enum Role {
 		Vetter, 
 		Freelancer,
+		BusinessDev,
 	}
 
  }
--- pallets/fellowship/src/traits.rs
+++ pallets/fellowship/src/traits.rs
@@ -1,4 +1,6 @@
 use sp_runtime::traits::BadOrigin;
+use frame_support::pallet_prelude::*;
+use codec::{FullCodec, FullEncode};
 
 /// The democracy handle is used to inititate the regular referenda for new applicants into the fellowship.
 pub trait DemocracyHandle<AccountId> {
@@ -11,20 +13,20 @@ pub trait DemocracyHandle<AccountId> {
 pub trait FellowshipHandle<AccountId> {
     type Role: Member
     + TypeInfo
-    + Default
     + MaxEncodedLen
     + FullCodec
     + FullEncode
     + Copy;
 
-    fn add_to_fellowship(who: AccountId, role: Self::Role) -> Result<(), DispatchError>;
-    fn revoke_fellowship(who: AccountId, slash_deposit: bool) -> Result<(), DispatchError>;
+    fn add_to_fellowship(who: &AccountId, role: Self::Role) -> Result<(), DispatchError>;
+    fn revoke_fellowship(who: &AccountId, slash_deposit: bool) -> Result<(), DispatchError>;
+    fn bulk_add_to_fellowship(shortlist: Vec<(AccountId, Self::Role)>) -> Result<(), DispatchError>;
 }
 
 pub trait EnsureRole<AccountId, Role> {
     type Success;
     fn ensure_role(acc: &AccountId, role: Role) -> Result<Self::Success, BadOrigin>;
-    fn ensure_fellowship(acc: &AccountId) -> Result<Self::Success, BadOrigin>;
+    fn ensure_role_in(acc: &AccountId, roles: Vec<Role>) -> Result<Self::Success, BadOrigin>;
 }
 
 
