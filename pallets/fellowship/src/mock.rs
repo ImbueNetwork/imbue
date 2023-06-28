2d64d1eb337db4b2258279fe5a3beea46523da67
f-gate <felixgate13@gmail.com>
2023-06-28 12:09:58 +0100

    fix

--- pallets/fellowship/src/mock.rs
+++ pallets/fellowship/src/mock.rs
@@ -1,17 +1,24 @@
 use crate as pallet_fellowship;
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::{traits::{ConstU16, ConstU64, Nothing}, pallet_prelude::*};
+use frame_support::once_cell::sync::Lazy;
+use frame_system::EnsureRoot;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
+	traits::{BlakeTwo256, IdentityLookup, IdentifyAccount, parameter_types, Verify},
 };
 use sp_core::sr25519::{Public, Signature};
-use frame_support::once_cell::sync::Lazy;
-use sp_std::convert::TryFrom;
+use sp_std::convert::{TryInto, TryFrom};
 use common_types::CurrencyId;
+use orml_traits::{MultiReservableCurrency, MultiCurrency};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
+type BlockNumber = u64;
+pub type Balance = u64;
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+ 
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -21,39 +28,90 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system,
-		TemplateModule: pallet_fellowship,
+		Fellowship: pallet_fellowship,
+        Tokens: orml_tokens,
 	}
 );
 
 impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = orml_tokens::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+parameter_types! {
+	pub MaxCandidatesPerShortlist: u32 = 100;
+	pub ShortlistPeriod: BlockNumber = 100;
+	pub MembershipDeposit: Balance = 50_000_000;
+	pub SlashAccount: AccountId = Public::from_raw([1u8; 32]);
+	pub BlockHashCount: BlockNumber = 250;
+
 }
 
 impl pallet_fellowship::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	type MultiCurrency = Tokens;
+	type ForceAuthority = EnsureRoot<AccountId>;
+	type DemocracyHandle = ();
+	type MaxCandidatesPerShortlist = MaxCandidatesPerShortlist;
+	type ShortlistPeriod = ShortlistPeriod;
+	type MembershipDeposit = MembershipDeposit;
+	type SlashAccount = SlashAccount;
+}
+
+impl crate::traits::DemocracyHandle<AccountId> for () {
+	fn initiate_shortlist_vote() -> () {
+		()
+	}
+	fn cancel_shortlist_vote() -> () {
+		()
+	}
+}
+
+orml_traits::parameter_type_with_key! {
+    pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+        100
+    };
+}
+
+parameter_types! {
+    pub const MaxReserves: u32 = 50;
+    pub MaxLocks: u32 = 2;
+}
+
+impl orml_tokens::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type Balance = Balance;
+    type Amount = i128;
+    type CurrencyId = common_types::CurrencyId;
+    type CurrencyHooks = ();
+    type WeightInfo = ();
+    type ExistentialDeposits = ExistentialDeposits;
+    type MaxLocks = MaxLocks;
+    type DustRemovalWhitelist = Nothing;
+    type MaxReserves = MaxReserves;
+    type ReserveIdentifier = [u8; 8];
 }
 
 pub static ALICE: Lazy<Public> = Lazy::new(|| Public::from_raw([125u8; 32]));
@@ -67,7 +125,7 @@ pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
 
     let mut ext = sp_io::TestExternalities::new(t);
     ext.execute_with(|| {
-        let initial_balance = 100_000_000_000u64;
+        let initial_balance = 100_000_000_000_000u64;
         System::set_block_number(1);
         let _ = Tokens::deposit(CurrencyId::Native, &ALICE, initial_balance);
         let _ = Tokens::deposit(CurrencyId::Native, &BOB, initial_balance);
