82313131cb1d281ace8db28b5898565708f3a278
cuteolaf <OliverLim818@gmail.com>
2023-07-03 01:23:57 -0700

    fix typo

--- pallets/proposals/src/runtime_api.rs
+++ pallets/proposals/src/runtime_api.rs
@@ -1,5 +1,5 @@
 sp_api::decl_runtime_apis! {
     pub trait ProjectsApi<AccountId> where AccountId: codec::Codec {
-        fn get_project_account_by_id(projet_id: u32) -> AccountId;
+        fn get_project_account_by_id(project_id: u32) -> AccountId;
     }
 }
