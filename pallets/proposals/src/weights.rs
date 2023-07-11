06f53a1a96fc524272cb533848b00e5750cb1bd1
cuteolaf <oliverlim818@gmail.com>
2023-07-11 14:32:42 +0200

    update weights

--- pallets/proposals/src/weights.rs
+++ pallets/proposals/src/weights.rs
@@ -21,79 +21,67 @@ pub trait WeightInfo {
 pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
     fn submit_milestone() -> Weight {
-        Weight::from_ref_time(51_777_000)
-            .saturating_add(Weight::from_proof_size(266244))
-            .saturating_add(T::DbWeight::get().reads(2))
-            .saturating_add(T::DbWeight::get().writes(4))
+        Weight::from_parts(752_706_000, 266244)
+            .saturating_add(T::DbWeight::get().reads(2_u64))
+            .saturating_add(T::DbWeight::get().writes(4_u64))
     }
     fn vote_on_milestone() -> Weight {
-        Weight::from_ref_time(65_447_000)
-            .saturating_add(Weight::from_proof_size(435823))
-            .saturating_add(T::DbWeight::get().reads(4))
-            .saturating_add(T::DbWeight::get().writes(3))
+        Weight::from_parts(973_777_000, 435823)
+            .saturating_add(T::DbWeight::get().reads(4_u64))
+            .saturating_add(T::DbWeight::get().writes(3_u64))
     }
     fn withdraw() -> Weight {
-        Weight::from_ref_time(136_071_000)
-            .saturating_add(Weight::from_proof_size(271107))
-            .saturating_add(T::DbWeight::get().reads(4))
-            .saturating_add(T::DbWeight::get().writes(4))
+        Weight::from_parts(2_547_270_000, 273651)
+            .saturating_add(T::DbWeight::get().reads(5_u64))
+            .saturating_add(T::DbWeight::get().writes(5_u64))
     }
     fn raise_vote_of_no_confidence() -> Weight {
-        Weight::from_ref_time(46_597_000)
-            .saturating_add(Weight::from_proof_size(436249))
-            .saturating_add(T::DbWeight::get().reads(4))
-            .saturating_add(T::DbWeight::get().writes(4))
+        Weight::from_parts(703_535_000, 436249)
+            .saturating_add(T::DbWeight::get().reads(4_u64))
+            .saturating_add(T::DbWeight::get().writes(4_u64))
     }
     fn vote_on_no_confidence_round() -> Weight {
-        Weight::from_ref_time(50_107_000)
-            .saturating_add(Weight::from_proof_size(435819))
-            .saturating_add(T::DbWeight::get().reads(4))
-            .saturating_add(T::DbWeight::get().writes(2))
+        Weight::from_parts(686_616_000, 435819)
+            .saturating_add(T::DbWeight::get().reads(4_u64))
+            .saturating_add(T::DbWeight::get().writes(2_u64))
     }
     fn finalise_no_confidence_round() -> Weight {
-        Weight::from_ref_time(168_434_000)
-            .saturating_add(Weight::from_proof_size(281282))
-            .saturating_add(T::DbWeight::get().reads(8))
-            .saturating_add(T::DbWeight::get().writes(6))
+        Weight::from_parts(2_454_429_000, 281282)
+            .saturating_add(T::DbWeight::get().reads(8_u64))
+            .saturating_add(T::DbWeight::get().writes(6_u64))
     }
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
     fn submit_milestone() -> Weight {
-        Weight::from_ref_time(51_777_000)
-            .saturating_add(Weight::from_proof_size(266244))
-            .saturating_add(RocksDbWeight::get().reads(2))
-            .saturating_add(RocksDbWeight::get().writes(4))
+        Weight::from_parts(752_706_000, 266244)
+            .saturating_add(RocksDbWeight::get().reads(2_u64))
+            .saturating_add(RocksDbWeight::get().writes(4_u64))
     }
     fn vote_on_milestone() -> Weight {
-        Weight::from_ref_time(65_447_000)
-            .saturating_add(Weight::from_proof_size(435823))
-            .saturating_add(RocksDbWeight::get().reads(4))
-            .saturating_add(RocksDbWeight::get().writes(3))
+        Weight::from_parts(973_777_000, 435823)
+            .saturating_add(RocksDbWeight::get().reads(4_u64))
+            .saturating_add(RocksDbWeight::get().writes(3_u64))
     }
     fn withdraw() -> Weight {
-        Weight::from_ref_time(136_071_000)
-            .saturating_add(Weight::from_proof_size(271107))
-            .saturating_add(RocksDbWeight::get().reads(4))
-            .saturating_add(RocksDbWeight::get().writes(4))
+        Weight::from_parts(2_547_270_000, 273651)
+            .saturating_add(RocksDbWeight::get().reads(5_u64))
+            .saturating_add(RocksDbWeight::get().writes(5_u64))
     }
     fn raise_vote_of_no_confidence() -> Weight {
-        Weight::from_ref_time(46_597_000)
-            .saturating_add(Weight::from_proof_size(436249))
-            .saturating_add(RocksDbWeight::get().reads(4))
-            .saturating_add(RocksDbWeight::get().writes(4))
+        Weight::from_parts(703_535_000, 436249)
+            .saturating_add(RocksDbWeight::get().reads(4_u64))
+            .saturating_add(RocksDbWeight::get().writes(4_u64))
     }
     fn vote_on_no_confidence_round() -> Weight {
-        Weight::from_ref_time(50_107_000)
-            .saturating_add(Weight::from_proof_size(435819))
-            .saturating_add(RocksDbWeight::get().reads(4))
-            .saturating_add(RocksDbWeight::get().writes(2))
+        Weight::from_parts(686_616_000, 435819)
+            .saturating_add(RocksDbWeight::get().reads(4_u64))
+            .saturating_add(RocksDbWeight::get().writes(2_u64))
     }
     fn finalise_no_confidence_round() -> Weight {
-        Weight::from_ref_time(168_434_000)
-            .saturating_add(Weight::from_proof_size(281282))
-            .saturating_add(RocksDbWeight::get().reads(8))
-            .saturating_add(RocksDbWeight::get().writes(6))
+        Weight::from_parts(2_454_429_000, 281282)
+            .saturating_add(RocksDbWeight::get().reads(8_u64))
+            .saturating_add(RocksDbWeight::get().writes(6_u64))
     }
 }
