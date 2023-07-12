adf0940702b8ab3799147c1da734b20f4a9ae515
f-gate <felixgate13@gmail.com>
2023-07-12 12:31:46 +0100

    rpc structure

--- pallets/proposals/rpc/runtime-api/src/lib.rs
+++ pallets/proposals/rpc/runtime-api/src/lib.rs
@@ -1,4 +1,5 @@
 sp_api::decl_runtime_apis! {
+    #[api_version(1)]
     pub trait ProjectsApi<AccountId> where AccountId: codec::Codec {
         fn get_project_account_by_id(project_id: u32) -> AccountId;
     }
