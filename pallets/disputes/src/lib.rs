40d8038f04a8a162745dbe1158beeeeec235c56d
f-gate <felixgate13@gmail.com>
2023-08-14 19:23:58 +0100

    structure pallet

--- pallets/disputes/src/lib.rs
+++ pallets/disputes/src/lib.rs
@@ -10,28 +10,31 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use traits::DisputeRaiser;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
 	pub(crate) type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 	pub(crate) type DisputeKey = u32;
-	pub(crate) type ProjectKey = u32;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type WeightInfo: WeightInfo;
+		type DisputeKey: AtLeast32BitUnsigned;
+		type MaxReasonLength: Get<u32>;
 	}
 
 	#[pallet::storage]
     pub type Disputes<T> =
-        StorageMap<_, Blake2_128Concat, ProjectKey, AccountIdOf<T>, OptionQuery>;
+        StorageMap<_, Blake2_128Concat, T::DisputeKey, Dispute<T>, OptionQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		DisputeRaised {who: AccountIdOf<T> },
+		DisputeRaised {who: AccountIdOf<T>},
+		DisputeVotedOn {who: AccountIdOf<T>},
 	}
 
 	#[pallet::error]
@@ -45,26 +48,41 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn handle_dispute(
+		pub fn vote_on_dispute(
 			origin: OriginFor<T>,
-            who: AccountIdOf<T>,
-			projectKey: ProjectKey) -> DispatchResult {
-
-			let origin = ensure_signed(origin)?;
-            Disputes::<T>::insert(projectKey,who.clone());
-
-
-			Self::deposit_event(Event::<T>::DisputeRaised {who: who.clone()});
-			Ok(())
+			dispute_key: T::DisputeKey,
+			is_yay: bool,
+		) -> DispatchResult {
+				Ok(().into())
 		}
-
+		#[pallet::call_index(1)]
+		#[pallet::weight(T::WeightInfo::do_something())]
+		pub fn force_cancel_dispute(
+			origin: OriginFor<T>,
+			dispute_key: T::DisputeKey,
+			is_yay: bool,
+		) -> DispatchResult {
+				Ok(().into())
+		}
 	}
 
-	#[derive(Encode, Decode, PartialEq, Eq, Copy, Clone, Debug, MaxEncodedLen)]
-    pub enum Jury {
-		Fellowship,
-		Contributors,
-		Canonical,
-    }
+	impl<T: Config> DisputeRaiser<AccountIdOf<T>> for Pallet<T> {
+		type DisputeKey = T::DisputeKey;
+		fn raise_dispute(
+			who: &AccountId,
+			reason: &str,
+			project_id: u32,
+		) -> Result<(), DispatchError> {
+			Ok(())
+		}
+	}
 
+	#[derive(Encode, Decode, PartialEq, Eq, Clone, Debug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	struct Dispute<T: Config> {
+		raised_by: AccountIdOf<T>,
+		fund_account: AccountIdOf<T>,
+		votes: todo!(),
+		reason: BoundedVec<u8, <T as Config>::MaxReasonLength>,
+	}
 }
--- pallets/disputes/src/traits.rs
+++ pallets/disputes/src/traits.rs
@@ -1,7 +1,12 @@
 use sp_runtime::DispatchError;
 
-pub trait DisputeRaiser<AccountId>{
+pub trait DisputeRaiser<AccountId> {
+    type DisputeKey: AtLeast32BitUnsigned;
 
+    // Strip this to be the minumim the dispute pallet needs to know.
+    // where is the money,
+    // Who is the jury,
+    // Bind the string to a constant amount (500)
     fn raise_dispute(
         who: &AccountId,
         reason: &str,
