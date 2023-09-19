2097d428f0fc325f614ab7aaa722044414f765b1
f-gate <felixgate13@gmail.com>
2023-09-19 20:20:36 +0100

    bump spec version

--- runtime/imbue-kusama/src/lib.rs
+++ runtime/imbue-kusama/src/lib.rs
@@ -98,7 +98,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("imbue"),
     impl_name: create_runtime_str!("imbue"),
     authoring_version: 2,
-    spec_version: 9430,
+    spec_version: 9431,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
