b0b30d74fe42146d6e6b1e99a7f679d8e27da446
f-gate <felixgate13@gmail.com>
2023-10-05 09:17:02 +0100

    -y

--- ci/jobs/install_rust.sh
+++ ci/jobs/install_rust.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 sudo apt -y install build-essential
-sudo apt -y install --assume-yes git clang curl libssl-dev
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt -y install --assume-yes git clang curl libssl-dev  
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
 source $HOME/.cargo/env
 rustup -y update
 rustup -y toolchain install nightly-2023-05-22
