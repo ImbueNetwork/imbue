6063cd8c64600ebfcd78b880288e93e937306c60
f-gate <felixgate13@gmail.com>
2023-10-05 09:06:41 +0100

    -y flag

--- ci/jobs/install_rust.sh
+++ ci/jobs/install_rust.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-sudo apt install build-essential
-sudo apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+sudo apt -y install build-essential
+sudo apt -y install --assume-yes git clang curl libssl-dev protobuf-compiler
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup update
