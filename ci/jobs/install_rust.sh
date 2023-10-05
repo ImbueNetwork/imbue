286a0d71ef69d95b4838341bf21725d5d06f731a
f-gate <felixgate13@gmail.com>
2023-10-05 09:28:05 +0100

    trial 1

--- ci/jobs/install_rust.sh
+++ ci/jobs/install_rust.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 sudo apt -y install build-essential
 sudo apt -y install --assume-yes git clang curl libssl-dev  
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup -y update
-rustup -y toolchain install nightly-2023-05-22
-rustup -y target add wasm32-unknown-unknown --toolchain nightly-2023-05-22
-rustup -y component add rustfmt
-rustup -y component add clippy
+#rustup -y toolchain install nightly-2023-05-22
+#rustup -y target add wasm32-unknown-unknown --toolchain nightly-2023-05-22
+#rustup -y component add rustfmt
+#rustup -y component add clippy
