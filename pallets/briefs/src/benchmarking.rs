07594dae06b0c11f18f0cb46b4bf478ed9b23b0c
Just van Stam <just.van.stam@gmail.com>
2023-11-03 15:50:11 +0100

    benchmark fix

--- pallets/briefs/src/benchmarking.rs
+++ pallets/briefs/src/benchmarking.rs
@@ -12,7 +12,7 @@ use orml_traits::MultiCurrency;
 use pallet_proposals::ProposedMilestone;
 use sp_arithmetic::per_things::Percent;
 use sp_runtime::SaturatedConversion;
-use sp_std::{convert::TryInto, str, vec::Vec};
+use sp_std::{convert::TryInto, str, vec::Vec, vec};
 
 const SEED: u32 = 0;
 
