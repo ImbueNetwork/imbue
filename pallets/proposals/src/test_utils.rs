5afef1baf04203080b47759b2e42db7c4d2ba3cc
f-gate <felixgate13@gmail.com>
2023-11-30 11:22:10 +0000

    fix benchmark build

--- pallets/proposals/src/test_utils.rs
+++ pallets/proposals/src/test_utils.rs
@@ -15,6 +15,7 @@ use sp_std::convert::TryInto;
 use frame_benchmarking::account;
 #[cfg(feature = "runtime-benchmarks")]
 use sp_std::vec::Vec;
+use frame_support::assert_ok;
 
 // pub fn run_to_block(n: BlockNumber) {
 //     while System::block_number() < n {
