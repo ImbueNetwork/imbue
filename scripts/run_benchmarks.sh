c7d1afff660c4669586008fb61453fbc93fec8a0
f-gate <felixgate13@gmail.com>
2023-12-08 15:18:46 +0000

    hmm

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -5,7 +5,6 @@ cd ../
 cargo build --locked --features runtime-benchmarks
 
 IMBUE=./target/debug/imbue
-ERR_FILE="benchmarking_err.txt"
 
 EXCLUDED_PALLETS=( 
     "frame_benchmarking"
@@ -31,21 +30,14 @@ for PALLET in "${PALLETS[@]}"; do
     WEIGHT_FILE="./pallets/${FOLDER}/src/weights.rs"
     echo "Benchmarking $PALLET with weight file $WEIGHT_FILE";
     
-    OUTPUT=$(
-        $IMBUE benchmark pallet \
-        --chain="imbue-dev" \
-        --steps=50 \
-        --repeat=20 \
-        --pallet="$PALLET" \
-        --extrinsic="*" \
-        --output="$WEIGHT_FILE" \
-        --template="./scripts/frame-weight-template.hbs" 2>&1
-    )
+    $IMBUE benchmark pallet \
+    --chain="imbue-dev" \
+    --steps=50 \
+    --repeat=20 \
+    --pallet="$PALLET" \
+    --extrinsic="*" \
+    --output="$WEIGHT_FILE" \
+    --template="./scripts/frame-weight-template.hbs" 2>&1
 
-    if [ $? -ne 0 ]; then
-      echo "$OUTPUT" >> "$ERR_FILE"
-      echo "[-] Failed to benchmark $PALLET. Error written to $ERR_FILE; continuing..."
-    fi
 done
-
 exit 0
